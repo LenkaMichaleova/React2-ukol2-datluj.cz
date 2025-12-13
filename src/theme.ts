7ff52fa778f6c9a221ded0db73ce4c0f46bda817
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-12-13 23:43:39 +0100

    installed MUI and removed css files, theme was created

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#b6c8db",
+      light: "#e3f2fd",
+      dark: "#e5d2b0",
+    },
+  },
+  typography: {
+    fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
+  },
+});
